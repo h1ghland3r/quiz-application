3d0c3878e17fba899eb834c0361a69efb1abb5c5
Railan Barbosa <railan.barbosa@osf-commerce.com>
2018-09-20 15:33:59 -0300

    Remove unnecessary jQuery call in the App.js

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,30 +1,28 @@
-$(document).ready(function () {
-    // define array of arabic to romans values
-    const romanValues = [
-        [1000, 'M'],
-        [900, 'CM'],
-        [500, 'D'],
-        [400, 'CD'],
-        [100, 'C'],
-        [90, 'XC'],
-        [50, 'L'],
-        [40, 'XL'],
-        [10, 'X'],
-        [9, 'IX'],
-        [5, 'V'],
-        [4, 'IV'],
-        [1, 'I']
-    ];
+// define array of arabic to romans values
+const romanValues = [
+    [1000, 'M'],
+    [900, 'CM'],
+    [500, 'D'],
+    [400, 'CD'],
+    [100, 'C'],
+    [90, 'XC'],
+    [50, 'L'],
+    [40, 'XL'],
+    [10, 'X'],
+    [9, 'IX'],
+    [5, 'V'],
+    [4, 'IV'],
+    [1, 'I']
+];
 
-    function convertToRoman(number) {
-        if (number > 0) {
-            for (i in romanValues) {
-                if (number >= romanValues[i][0]) {
-                    return romanValues[i][1] + convertToRoman(number - romanValues[i][0]);
-                }
+function convertToRoman(number) {
+    if (number > 0) {
+        for (i in romanValues) {
+            if (number >= romanValues[i][0]) {
+                return romanValues[i][1] + convertToRoman(number - romanValues[i][0]);
             }
-        } else {
-            return '';
         }
+    } else {
+        return '';
     }
-});
+}
